22fa1f2c86b6f9ffb4672e90165cce581493bc25
Cheryl K <cherylkelson@icloud.com>
2018-09-20 16:25:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,8 @@ def key_for_min_value(name_hash)
   new_hash = {}
   name_hash.each do |key, value|
    
+  
+  def lowest_key(key, value)
   if  lowest_key == nil || lowest_value == nil || value < lowest_value && lowest_key = key 
     lowest_value = value
    new_hash[key] = value 
