cd37ffd6967015459e72aa0fbd2d0d48bc93123d
Cheryl K <cherylkelson@icloud.com>
2018-09-20 17:42:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,9 +7,9 @@ def key_for_min_value(hash_name)
   new_hash[key] = value 
     hash_name.each do |key, value|
     if value < new_hash
-      
+      new_hash
     else
-        end
+      nil
     end
 
 end
