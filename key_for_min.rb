0b1d43d0d6d6babe4587efae1431acff86e89615
Cheryl K <cherylkelson@icloud.com>
2018-09-20 17:26:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,10 @@ def key_for_min_value(hash_name)
   new_hash = {}
   new_hash[key] = value 
     hash_name.each do |key, value|
-    if lowest_key == nil || lowest_value == nil ||value <lowest_value
-        
+    if lowest_key == nil || lowest_value == nil || value < lowest_value
+        key_for_min_value
     else # Keep re-setting the local variable lowest_key until I find the lowest one
-          do
+          
 
 
 end
