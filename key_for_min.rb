469c7b1602c977ada0f741da7652188184e918d5
Cheryl K <cherylkelson@icloud.com>
2018-09-20 17:07:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,11 @@ require 'pry'
 def key_for_min_value(name_hash)
   new_hash = {}
   new_hash[key] = value 
-  lowest_key = highest_key -1
+    
   
  
 #   name_hash.each do |key, value|
-#     if value < lowest_value && key < lowest_key 
+
      
 #     else
     
